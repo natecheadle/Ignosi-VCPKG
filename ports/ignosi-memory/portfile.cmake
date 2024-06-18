5a42bea87a9e6f2ccdb5871e3cf7ccdf68acb0be
Nathan Cheadle <natecheadle@gmail.com>
2024-06-17 22:12:25 -0700

    [ignosi-memory] fix port

--- ports/ignosi-memory/portfile.cmake
+++ ports/ignosi-memory/portfile.cmake
@@ -4,9 +4,9 @@ vcpkg_from_github(
   REPO
   natecheadle/Ignosi-Memory
   REF
-  7fd27c91a2a50a2a37b348c754886d4bfe72fa0b
+  4c7cf5939fca0fbe70090b1f45b4d3d55f45e433
   SHA512
-  85c8fe213311f599bc167a14acc1189c62f365adbbae27a3e9285938d505d768a7efac3cdd909625f3db1ecb6080f92fae1ba53bc12229eebdf0e7391be18189
+  73716a4673998e57e524155dbb59d9646602370fec0e77fff49d713149cc46d1702156946c6a377437ca26d3b89c1fdbd1d188a3214e36989fdf1c936122ffda
   HEAD_REF
   master)
 
@@ -17,21 +17,10 @@ vcpkg_copy_pdbs()
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/ignosi-memory/LICENSE
-     ${CURRENT_PACKAGES_DIR}/share/ignosi-memory/copyright)
-file(GLOB CMAKE_EXPORT_FILES_IGNOSI_MEMORY
-     "${CURRENT_PACKAGES_DIR}/lib/cmake/ignosi-memory/*")
-file(GLOB CMAKE_EXPORT_FILES_IGNOSI_MEMORY_DEBUG
-     "${CURRENT_PACKAGES_DIR}/debug/lib/cmake/ignosi-memory/*")
-
-file(COPY ${CMAKE_EXPORT_FILES_IGNOSI_MEMORY}
-     DESTINATION ${CURRENT_PACKAGES_DIR}/share/ignosi-memory/cmake)
-file(COPY ${CMAKE_EXPORT_FILES_IGNOSI_MEMORY_DEBUG}
-     DESTINATION ${CURRENT_PACKAGES_DIR}/share/ignosi-memory/cmake)
-
-vcpkg_cmake_config_fixup()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
